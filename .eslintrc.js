12a36b588cbd2fff8c3ed8915b90511d57c8621d
Yisaaaa <ryansanisit19@gmail.com>
2023-12-31 01:59:54 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-tabs */
+module.exports = {
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: "airbnb-base",
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: "latest",
+	},
+	rules: {
+		indent: ["error", "tab"],
+		quotes: ["error", "double"],
+		"no-tabs": 0,
+	},
+};
