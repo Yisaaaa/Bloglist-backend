e5c2a4a36c6004f61e8b3af4c3b48c7038bb9f12
Yisaaaa <ryansanisit19@gmail.com>
2023-12-31 20:01:39 +0800

    update on how config is being used

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
-const { PORT } = config.PORT;
+const PORT = config.PORT;
 
 app.listen(PORT, () => {
 	logger.info(`Server running on port ${PORT}`);
