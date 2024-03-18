1dbe88984e09c9d7132f7560cec628153edb510f
Yisaaaa <ryansanisit19@gmail.com>
2024-03-18 20:15:40 +0800

    added .env cause it's such a hassle to setup when i coding in different machine

--- controllers/user.js
+++ controllers/user.js
@@ -3,47 +3,47 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 usersRouter.get("/", async (request, response) => {
-	const users = await User.find({}).populate("blogs", {
-		title: 1,
-		url: 1,
-		author: 1,
-	});
+    const users = await User.find({}).populate("blogs", {
+        title: 1,
+        url: 1,
+        author: 1,
+    });
 
-	response.json(users);
+    response.json(users);
 });
 
 usersRouter.get("/:id", async (request, response) => {
-	const id = request.params.id;
-	const user = await User.findById(id).populate("blogs", {
-		title: 1,
-		url: 1,
-		author: 1,
-	});
+    const id = request.params.id;
+    const user = await User.findById(id).populate("blogs", {
+        title: 1,
+        url: 1,
+        author: 1,
+    });
 
-	response.json(user);
+    response.json(user);
 });
 
 usersRouter.post("/", async (request, response) => {
-	const { username, name, password } = request.body;
+    const { username, name, password } = request.body;
 
-	if (password.length < 3) {
-		response
-			.status(400)
-			.json({ error: "password should be at least 3 characters long" });
-	}
+    if (password.length < 3) {
+        response
+            .status(400)
+            .json({ error: "password should be at least 3 characters long" });
+    }
 
-	const saltRounds = 10;
-	const passwordHash = await bcrypt.hash(password, saltRounds);
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-	const user = new User({
-		username,
-		name,
-		passwordHash,
-	});
+    const user = new User({
+        username,
+        name,
+        passwordHash,
+    });
 
-	const savedUser = await user.save();
+    const savedUser = await user.save();
 
-	response.status(201).json(savedUser);
+    response.status(201).json(savedUser);
 });
 
 module.exports = usersRouter;
